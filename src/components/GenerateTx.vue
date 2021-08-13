eef6f29e26fdb27eb66ef317dd7f34d75d3cba30
imollov <ivomollov@gmail.com>
2021-08-13 16:33:02 +0300

    feat: generate contract call tx

---
+++ src/components/GenerateTx.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="generate-tx">
+    <form @submit.prevent="handleSubmit">
+      <input v-model="fromAlias" type="text" placeholder="From name" required />
+      <input v-model="toAddress" type="text" placeholder="To" required />
+      <br />
+      <div>
+        <label>
+          <input v-model="txType" type="radio" value="spend" />
+          Spend
+        </label>
+        <label>
+          <input v-model="txType" type="radio" value="contractCall" />
+          Contract Call
+        </label>
+      </div>
+      <br />
+      <div v-if="txType === 'spend'">
+        <input
+          v-model="amount"
+          type="text"
+          placeholder="Amount"
+          required
+        />usrct
+      </div>
+      <div v-else-if="txType === 'contractCall'">
+        <textarea
+          v-model="message"
+          cols="40"
+          rows="5"
+          placeholder="Message"
+          required
+        ></textarea>
+      </div>
+      <button type="submit" :disabled="loading">Generate</button>
+    </form>
+    <br />
+    <textarea v-if="output" v-model="output" cols="40" rows="20"></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { generateSendTx, generateContractCallTx } from "@/utils/secretcli";
+
+enum TxType {
+  Spend = "spend",
+  ContractCall = "contractCall",
+}
+
+export default defineComponent({
+  data() {
+    return {
+      txType: TxType.Spend,
+      fromAlias: "",
+      toAddress: "",
+      amount: "",
+      message: "",
+      output: "",
+      loading: false,
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      this.loading = true;
+      try {
+        let result = "";
+        if (this.txType === TxType.Spend) {
+          result = await generateSendTx(
+            this.fromAlias,
+            this.toAddress,
+            +this.amount
+          );
+        } else if (this.txType === TxType.ContractCall) {
+          result = await generateContractCallTx(
+            this.fromAlias,
+            this.toAddress,
+            this.message
+          );
+        }
+        this.output = result;
+      } catch (error) {
+        alert(error.message || "Error");
+      }
+      this.loading = false;
+    },
+  },
+});
+</script>
--- src/utils/secretcli.ts
+++ src/utils/secretcli.ts
@@ -1,17 +1,48 @@
 import * as child from "child_process";
 
-export function generateMultisigSendTx(
+export function generateSendTx(
   fromAlias: string,
   to: string,
   amount: number
 ): Promise<string> {
+  return promisifyExec(
+    `secretcli tx send $(secretcli keys show -a ${fromAlias}) ` +
+      `${to} ${amount}usrct --generate-only`
+  );
+}
+
+export async function generateContractCallTx(
+  fromAlias: string,
+  contract: string,
+  message: string
+): Promise<string> {
+  await getCertificates();
+  const contractHash = await getContractHash(contract);
+  return promisifyExec(
+    `secretcli tx compute execute ${contract} '${message}' ` +
+      "--generate-only " +
+      `--from $(secretcli keys show -a ${fromAlias}) ` +
+      `--code-hash ${contractHash} ` +
+      "--enclave-key io-master-cert.der"
+  );
+}
+
+async function getContractHash(contract: string) {
+  const result = await promisifyExec(
+    `secretcli q compute contract-hash ${contract}`
+  );
+  return result.slice(2); // remove 0x
+}
+
+function getCertificates(): Promise<string> {
+  return promisifyExec("secretcli query register secret-network-params");
+}
+
+function promisifyExec(command: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    child.exec(
-      `secretcli tx send $(secretcli keys show -a ${fromAlias}) ${to} ${amount}usrct --generate-only`,
-      (error, data) => {
-        if (error) reject(error);
-        resolve(data);
-      }
-    );
+    child.exec(command, (error, data) => {
+      if (error) reject(error);
+      resolve(data);
+    });
   });
 }
